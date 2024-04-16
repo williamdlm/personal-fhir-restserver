dca6bd3b8ef5c540c6cec836cf3745a62386d8ab
William Mota <william.mota.ext@spms.min-saude.pt>
2024-04-16 23:53:00 +0100

    Ajuste para chamar o BaseRestfulServelet na inicialização da aplicação e configura alguns parametros da inicializacao

--- src/main/java/com/github/williamdlm/personalfhirserver/PersonalFhirServerApplication.java
+++ src/main/java/com/github/williamdlm/personalfhirserver/PersonalFhirServerApplication.java
@@ -1,14 +1,33 @@
 package com.github.williamdlm.personalfhirserver;
 
+import com.github.williamdlm.personalfhirserver.config.BaseRestfulServlet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {ElasticsearchRestClientAutoConfiguration.class})
 public class PersonalFhirServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonalFhirServerApplication.class, args);
-	}
+    @Autowired
+    AutowireCapableBeanFactory beanFactory;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalFhirServerApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean<BaseRestfulServlet> hapiServletRegistration() {
+        ServletRegistrationBean<BaseRestfulServlet> servletRegistrationBean = new ServletRegistrationBean<>();
+        BaseRestfulServlet plainRestfulServer = new BaseRestfulServlet();
+        beanFactory.autowireBean(plainRestfulServer);
+        servletRegistrationBean.setServlet(plainRestfulServer);
+        servletRegistrationBean.addUrlMappings("/fhir/*");
+        servletRegistrationBean.setLoadOnStartup(1);
+        return servletRegistrationBean;
+    }
 
 }
