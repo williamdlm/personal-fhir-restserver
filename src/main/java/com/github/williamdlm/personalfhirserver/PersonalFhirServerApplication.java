211bb12bf5d655a144d9cff2136d451457838c00
William Mota <william.mota.ext@spms.min-saude.pt>
2024-04-16 01:12:51 +0100

    Initial commit

---
+++ src/main/java/com/github/williamdlm/personalfhirserver/PersonalFhirServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.personalfhirserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFhirServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFhirServerApplication.class, args);
+	}
+
+}
