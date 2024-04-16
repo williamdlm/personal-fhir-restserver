23df40e79d9726bf3901b41cd83fb19d2e514636
William Mota <william.mota.ext@spms.min-saude.pt>
2024-04-16 01:51:48 +0100

    Ajuste para evitar utilizar elasticsearch

--- src/main/java/com/github/williamdlm/personalfhirserver/PersonalFhirServerApplication.java
+++ src/main/java/com/github/williamdlm/personalfhirserver/PersonalFhirServerApplication.java
@@ -2,8 +2,9 @@ package com.github.williamdlm.personalfhirserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchRestClientAutoConfiguration.class})
 public class PersonalFhirServerApplication {
 
 	public static void main(String[] args) {
